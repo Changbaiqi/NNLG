afb6aa47519b0f66c78d7df8d4afb50db4470074
Changbaiqi <2084069833@qq.com>
2023-08-11 18:46:56 +0800

    更新相关功能代码

--- lib/dao/ContextData.dart
+++ lib/dao/ContextData.dart
@@ -6,9 +6,9 @@ class ContextDate{
 
   static String ContextUrl = 'http://bwgljw.yinghuaonline.com';
   //static String VIPContextUrl = 'http://192.168.1.10:8088';
-  static String VIPContextUrl = 'https://www.changbaiqi.com';
+  static String VIPContextUrl = 'https://www.changbaiqi.top';
   // static String VIPContextIpPort = 'ws://172.29.40.104:8088';
-  static String VIPContextIpPort = 'wss://www.changbaiqi.com';
+  static String VIPContextIpPort = 'wss://www.changbaiqi.top';
   static String ContextCookie = '';
   static String ContextVIPTken = '';
 

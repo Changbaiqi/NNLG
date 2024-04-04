db17cc023c52f8135980e7eab3beeea4c46421d6
Changbaiqi <changbaiqi@foxmail.com>
2024-04-04 16:46:14 +0800

    添加新课表显示组件依赖

--- lib/view/main_course_new_view/view.dart
+++ lib/view/main_course_new_view/view.dart
@@ -13,9 +13,64 @@ class MainCourseNewViewPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: SfCalendar(),
-      ),
-    );
+        body: SfCalendar(
+          view: CalendarView.week,
+          timeSlotViewSettings: TimeSlotViewSettings(
+              startHour: 8,
+              endHour: 22,
+              nonWorkingDays: <int>[DateTime.friday, DateTime.saturday]),
+        ));
   }
+
+  List<Meeting> _getDataSource() {
+    final List<Meeting> meetings = <Meeting>[];
+    final DateTime today = DateTime.now();
+    final DateTime startTime =
+        DateTime(today.year, today.month, today.day, 9, 0, 0);
+    final DateTime endTime = startTime.add(const Duration(hours: 2));
+    meetings.add(Meeting(
+        'Conference', startTime, endTime, const Color(0xFF0F8644), false));
+    return meetings;
+  }
+}
+
+class MeetingDataSource extends CalendarDataSource {
+  MeetingDataSource(List<Meeting> source) {
+    appointments = source;
+  }
+
+  @override
+  DateTime getStartTime(int index) {
+    return appointments![index].from;
+  }
+
+  @override
+  DateTime getEndTime(int index) {
+    return appointments![index].to;
+  }
+
+  @override
+  String getSubject(int index) {
+    return appointments![index].eventName;
+  }
+
+  @override
+  Color getColor(int index) {
+    return appointments![index].background;
+  }
+
+  @override
+  bool isAllDay(int index) {
+    return appointments![index].isAllDay;
+  }
+}
+
+class Meeting {
+  Meeting(this.eventName, this.from, this.to, this.background, this.isAllDay);
+
+  String eventName;
+  DateTime from;
+  DateTime to;
+  Color background;
+  bool isAllDay;
 }

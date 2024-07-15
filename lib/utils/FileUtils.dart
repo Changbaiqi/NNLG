78d19c69538223ca202eb7ad15210bcce66c809e
Changbaiqi <changbaiqi@foxmail.com>
2024-07-15 17:39:02 +0800

    添加工具类

---
+++ lib/utils/FileUtils.dart
@@ -0,0 +1,16 @@
+/* FileName FileUtils
+ *
+ * @Author 20840
+ * @Date 2024/7/14 23:27
+ *
+ * @Description TODO
+ */
+import 'dart:convert';
+import 'package:flutter/services.dart';
+
+class FileUtils{
+  static Future<Map<String, dynamic>> loadJsonFromAssets(String filePath) async {
+    String jsonString = await rootBundle.loadString(filePath);
+    return jsonDecode(jsonString);
+  }
+}

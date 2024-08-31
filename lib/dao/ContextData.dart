255d9c56ee41e5fdb7a2de29e15aa903a6ca6226
Changbaiqi <changbaiqi@foxmail.com>
2024-08-31 12:47:16 +0800

    更新学校官网域名

--- lib/dao/ContextData.dart
+++ lib/dao/ContextData.dart
@@ -5,7 +5,7 @@ import 'package:get/get.dart';
 
 class ContextDate{
 
-  static const String ContextUrl = 'http://bwgljw.yinghuaonline.com';
+  static const String ContextUrl = 'http://bwgljw.minghuaetc.com';
   //static String VIPContextUrl = 'http://192.168.1.10:8088';
   static const String VIPContextUrl = 'https://www.changbaiqi.top';
   static String ContextVIPTken = '';

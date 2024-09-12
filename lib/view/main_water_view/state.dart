0face5a238d3ec0b7b1aa2f46341e1eb0a9b8146
Changbaiqi <changbaiqi@foxmail.com>
2024-09-12 17:35:34 +0800

    添加Location数据

--- lib/view/main_water_view/state.dart
+++ lib/view/main_water_view/state.dart
@@ -4,6 +4,10 @@ class MainWaterViewState {
   final money= "".obs;
   final divice="".obs;
   final bingCard="".obs;
+  final longitude = 0.0.obs;
+  final latitude = 0.0.obs;
+  final altitude = 0.0.obs;
+  final altitudeAccuracy = 0.0.obs;
   MainWaterViewState() {
     ///Initialize variables
   }
